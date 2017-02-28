458ab2f5a416dc787d25e840e79574702cc5f940
David Bonner <david@continuuminteractive.com>
2017-02-28 21:29:13 +0000

    Initial comm it

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# spanvg
-S[pan]VG – load up your SVG icons and images with a simple &lt;span>
+# Easy SVG
+
+###### N.B. This plugin is very much in it's infancy and was put together to handle my personal needs with a project. Both tests and build scripts are coming soon. A CSS stylesheet will be included shortly as well.
+
+## How easy?
+
+@todo
+
+## Usage
+
+#### Step 1
+
+Include jQuery (but you knew that already).
+
+#### Step 2
+
+Include the plugin:
+
+```html
+<script src="dist/jquery.easy-svg.min.js"></script>
+```
+
+#### Step 3
+
+Set the path to the SVG sprite sheet. Sprite sheets can be generated at https://icomoon.io/ or put together manually.
+
+```html
+// 'window.$svgpath' can be a string
+$.fn.svgIcon.options.path = window.$svgpath;
+```
+
+#### Step 4
+
+Use `[data-icon="svg-id"]` to initialise the icons. The `data-icon` attribute takes the ID associated with the symbol within the SVG document.
+
+```javascript
+<span class="my-icon-class" data-icon="star"></span>
+```
+
+## Contributing
+
+Check [CONTRIBUTING.md](https://github.com/dbonner1987/easy-svg/blob/master/CONTRIBUTING.md) for more information.
+
+## License
+
+[MIT License](https://dbonner1987.mit-license.org/) © David Bonner
---
+++ jquery.easy-svg.js
@@ -0,0 +1,63 @@
+'use strict';
+
+(function ($) {
+  $.fn.svgIcon = function(options) {
+    var opts = $.extend({}, $.fn.svgIcon.options, options);
+
+    if ($.fn.svgIcon.loading.indexOf(opts.path) > -1) {
+      return;
+    }
+
+    if (!$.fn.svgIcon.icons) {
+      $.fn.svgIcon.loading.push(opts.path);
+      loadSvg(opts);
+      return;
+    }
+
+    this.each(function () {
+      if ($(this).children('svg').length > 0) {
+        return;
+      }
+
+      var name    = $(this).attr('data-icon'),
+          svg     = $.fn.svgIcon.icons.getElementById(name),
+          classes = $(this).attr('data-icon-class') + ' iname-' + name;
+
+      if (svg) {
+        svg = svg.outerHTML;
+
+        var html = svg.replace(/<symbol/g, "<svg class=\"${classes}\" ${!~svg.indexOf('xmlns') ? ' xmlns=\"http://www.w3.org/2000/svg\"' : ''}").replace(/symbol>/g, 'svg>');
+        $(this).append(html);
+      }
+    });
+
+    return this;
+  };
+
+  function loadSvg(options) {
+    // Without $(window).on('load') this method is called very
+    // early so the options path has not been set yet.
+    if (!options.path) {
+      return;
+    }
+
+    $.get(options.path, function (response) {
+      $.fn.svgIcon.loading.splice($.fn.svgIcon.loading.indexOf(options.path), 1);
+      $.fn.svgIcon.icons = response;
+      $('[data-icon]').svgIcon();
+    });
+  }
+
+  $.fn.svgIcon.icons = null;
+  $.fn.svgIcon.loading = new Array;
+
+  $.fn.svgIcon.options = {
+    path: null,
+    classes: '',
+  };
+
+  // $(window).on('load', function () {
+    $('[data-icon]').svgIcon();
+  // });
+
+}(jQuery));
